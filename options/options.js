0a8400c7ea64e0ae606432c487edb8f471968d92
Crowdbotics <team@crowdbotics.com>
2023-11-29 05:14:33 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "falling-mouse-44792",
+  url: "https://falling-mouse-44792.botics.co",
+  api: "https://falling-mouse-44792.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
